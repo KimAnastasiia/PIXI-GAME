cb2b56d34dec5440580c7dfffc8835bdd3c4b566
Anastasiia <anstasiakim2789@gmail.com>
2023-09-10 21:04:17 +0200

    The physical body of the hero

--- src/scripts/game/Hero.js
+++ src/scripts/game/Hero.js
@@ -1,11 +1,18 @@
 import * as PIXI from "pixi.js";
 import { App } from '../system/App';
-
+import * as Matter from 'matter-js';
 export class Hero {
     constructor() {
         this.createSprite();
-    }
 
+        this.createBody();
+        App.app.ticker.add(this.update.bind(this));
+    }
+    createBody() {
+        this.body = Matter.Bodies.rectangle(this.sprite.x + this.sprite.width / 2, this.sprite.y + this.sprite.height / 2, this.sprite.width, this.sprite.height, {friction: 0});
+        Matter.World.add(App.physics.world, this.body);
+        this.body.gameHero = this;
+    }
     createSprite() {
         this.sprite = new PIXI.AnimatedSprite([
             App.res("walk1"),
@@ -17,5 +24,9 @@ export class Hero {
         this.sprite.loop = true;
         this.sprite.animationSpeed = 0.2;
         this.sprite.play();
+    }
+    update() {
+        this.sprite.x = this.body.position.x - this.sprite.width / 2;
+        this.sprite.y = this.body.position.y - this.sprite.height / 2;
     }
 }
